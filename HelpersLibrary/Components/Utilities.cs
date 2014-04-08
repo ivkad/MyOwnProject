3aa4126f55c3cb2d54c6f65930b36b3318cb35fd
ivkad <ivan.kadev@excelian.com>
2014-04-08 12:28:22 +0100

    commit some initial files

---
+++ HelpersLibrary/Components/Utilities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpersLibrary.Components
+{
+    public class Utilities
+    {
+        public Utilities()
+        {
+            prices = new List<Decimal>()
+            {
+                12.5m,
+                13.5m
+            };
+        }
+        //properties
+        public bool BoolProp1 { get; set; }
+
+        public String StringProp1 { get; set; }
+
+        public int IntProp1 { get; set; }
+
+        public List<Decimal> prices; 
+
+        public bool DoSomething()
+        {
+            BoolProp1 = true;
+            StringProp1 = "something";
+
+            prices.Add(20);
+
+            return true;
+        }
+
+        public List<Decimal> GetPrices()
+        {
+            return prices;
+        }
+    }
+}
---
+++ HelpersLibrary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpersLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
